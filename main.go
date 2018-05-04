fb8eb88dccfc95250e93e7e8f34b98f86bc78024
imrojony <imrojony@gmail.com>
2018-05-04 21:50:14 +0600

    initial

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK,"hello from %v", "gin framework")
+	})
+
+	r.Run(":4200")
+}
